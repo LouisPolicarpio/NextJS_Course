e6102fd6564f2e149631af65b67085215c221ca8
LouisPolicarpio <louis.policarpio@gmail.com>
2024-03-04 19:35:19 +1100

    tut7-Dynamic routes

---
+++ routing-demo/src/app/products/page.tsx
@@ -0,0 +1,10 @@
+export default function products() {
+  return (
+    <>
+      <h1>product list</h1>
+      <h2>product 1</h2>
+      <h2>product 2</h2>
+      <h2>product 3</h2>
+    </>
+  );
+}
