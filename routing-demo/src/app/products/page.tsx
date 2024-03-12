11ab575a715cf0c9d42d6e4097475f13ea2d744c
LouisPolicarpio <louis.policarpio@gmail.com>
2024-03-12 16:19:16 +1100

    tut 19 link component navigationion

--- routing-demo/src/app/products/page.tsx
+++ routing-demo/src/app/products/page.tsx
@@ -1,10 +1,24 @@
+import Link from "next/link";
 export default function products() {
+  const productID = 100;
   return (
     <>
       <h1>product list</h1>
-      <h2>product 1</h2>
-      <h2>product 2</h2>
-      <h2>product 3</h2>
+      <h2>
+        <Link href={"products/1"}>product 1</Link>
+      </h2>
+      <h2>
+        <Link href={"products/2"}>product 2</Link>
+      </h2>
+      <h2>
+        <Link href={"products/3"} replace>
+          product 3
+        </Link>
+      </h2>
+      <h2>
+        <Link href={`products/${productID}`}> products{productID} </Link>
+      </h2>
+      <Link href={`/`}> Home</Link>
     </>
   );
 }
